f87782f65b6282adcf490506c1eb52c08d77b992
Md. Jony Islam <jony.just.cse@gmail.com>
2021-11-06 00:25:53 +0600

    jwt login method

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,16 @@ Route::ApiResource('/class','Api\SclassController');
 Route::ApiResource('/subject','Api\SubjectController');
 Route::ApiResource('/section','Api\SectionController');
 Route::ApiResource('/student','Api\StudentController');
+
+
+
+Route::group([
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/refresh', 'AuthController@refresh');
+    Route::get('/user-profile', 'AuthController@userProfile');    
+});
