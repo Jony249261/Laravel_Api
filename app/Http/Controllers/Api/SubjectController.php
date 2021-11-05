a609025895771888d9a02a61a8e0227f6e9a1fe9
Md. Jony Islam <jony.just.cse@gmail.com>
2021-11-05 22:03:49 +0600

    subject

---
+++ app/Http/Controllers/Api/SubjectController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Model\Subject;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Subject::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData=$request->validate([
+            'class_id' => 'required',
+            'subject_name' => 'required|unique:subjects|max:25',
+            'subject_code' => 'required|unique:subjects|max:25',
+        ]);
+
+        //$data = Subject::
+        $data = new Subject();
+        $data->class_id = $request->class_id;
+        $data->subject_name = $request->subject_name;
+        $data->subject_code = $request->subject_code;
+        $data->save();
+        return response('Inserted Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Subject::findorfail($id);
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validateData=$request->validate([
+            'class_id' => 'required',
+            'subject_name' => 'required|unique:subjects|max:25',
+            'subject_code' => 'required|unique:subjects|max:25',
+        ]);
+
+        //$data = Subject::
+        $data = Subject::findorfail($id);
+        $data->class_id = $request->class_id;
+        $data->subject_name = $request->subject_name;
+        $data->subject_code = $request->subject_code;
+        $data->save();
+        return response('Inserted Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Subject::where('id',$id)->delete();
+        return response('Data Deleted Successfully');
+    }
+}
